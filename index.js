3f7076929f9ff2b3ccdf847f4f5c868e4a2b3d01
Jesse Chan <j.chan092@gmail.com>
2023-11-12 14:35:37 -0800

    Initial commit

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ function add (a, b) {
   return a + b
 }
 
-console.log(add(20, 30))
-
-
 
 const subtract = ((a, b) => a - b)
 
-console.log(subtract(24, 4))
-
 const multiply = ((a, b) => a * b)
 
-console.log(multiply(2, 4))
-
 const divide = ((a, b) => a / b)
-
-console.log(divide(40, 10))
-
-
-2 + 2 
 
 const firstNumber = a 
 
