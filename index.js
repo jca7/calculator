24b006dfb15017080b1a7d7e617f1db9628f5749
Jesse Chan <j.chan092@gmail.com>
2023-11-13 09:43:44 -0800

    Added listeners for clicks and button animations

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ function add (a, b) {
   return a + b
 }
 
-
 const subtract = ((a, b) => a - b)
 
 const multiply = ((a, b) => a * b)
 
 const divide = ((a, b) => a / b)
 
-const firstNumber = a 
+const firstNumber = 'a'
 
-const secondNumber = b 
+const secondNumber = 'b' 
 
 const operator  = + 
-
 
 function operate(operator, a, b) {
   if (operator === '+') {
@@ -30,3 +28,28 @@ function operate(operator, a, b) {
     divide(a, b)
   }
 } 
+
+
+
+for(i = 0; i < document.querySelectorAll(".flexButton").length; i++) {
+  document.querySelectorAll(".flexButton")[i].addEventListener("click", function () { // this function gets called after listening for click. 
+    var buttonInnerHTML = this.textContent; 
+    console.log(buttonInnerHTML)
+    buttonAnimation(this);
+    
+  })
+} 
+
+
+function buttonAnimation(button) {
+  button.classList.add("pressed");
+  setTimeout(function () {
+    button.classList.remove("pressed");
+  }, 150);
+}
+
+
+const displayNumbers = document.getElementById("numbers");
+
+// Set the text content of the <p> element
+displayNumbers.textContent = "3245";
